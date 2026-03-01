e6be6e5a1a52e007b6c2cf636d9deaf1f62d4b32
Avilash Basu <basu.abhilash@gmail.com>
2026-03-01 10:17:25 +0530

    Some chanfes

--- pipeline/deploy-dev.sh
+++ pipeline/deploy-dev.sh
@@ -14,3 +14,4 @@ echo "Authorizing org"
 sf org login jwt --client-id $SF_CONSUMER_KEY --jwt-key-file server.key --username $SF_USERNAME --instance-url $SF_INSTANCE_URL
 
 echo "Deploying Metadata"
+sf project deploy start --manifest=manifest/package.xml --target-org $SF_USERNAME
