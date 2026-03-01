51d2c596015109028b33a9113ac118fde6ec2494
Avilash Basu <basu.abhilash@gmail.com>
2026-03-01 10:10:44 +0530

    Changes

--- pipeline/deploy-dev.sh
+++ pipeline/deploy-dev.sh
@@ -1,9 +1,16 @@
 set -e
 
 echo "Installing Salesforce Cli"
+npm install @salesforce/cli --global
 
 echo "Checking if CLI installed"
+sf version
+
+echo "Decoding JWT Key"
+echo "$SF_JWT_KEY" | base64 --decode > server.key
+
 
 echo "Authorizing org"
+f org login jwt --client-id $SF_CONSUMER_KEY --jwt-key-file server.key --username $SF_USERNAME --instance-url $SF_INSTANCE_URL
 
 echo "Deploying Metadata"
